a83a89991ac1880b9f64e6c5ebea4df9ce0f5746
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-27 18:34:43 -0300

    Get data from API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-export default function User({ navigation }) {
-  return (
-    <View>
-      <Text>{navigation.getParam('User')}</Text>
-    </View>
-  );
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
