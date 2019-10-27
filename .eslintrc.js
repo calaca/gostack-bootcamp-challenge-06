1de8e027c99b41006fb897fab4840363f350b4f6
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-27 17:08:32 -0300

    Accessing GitHub API

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,5 +36,6 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'react/prefer-stateless-function': 'off'
   },
 };
